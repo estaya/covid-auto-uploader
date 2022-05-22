f951e7a1b184695c9d7d612999badf756c99b6f9
estaya <estaya@sustech.edu.cn>
2022-05-22 09:59:16 +0800

    release browser when encounter error

--- index.js
+++ index.js
@@ -42,63 +42,65 @@ async function submit(covid_img) {
     console.log(covid_id);
 
     const browser = await puppeteer.launch({ headless: false, ignoreDefaultArgs: ["--enable-automation"] });
-    const page = await browser.newPage();
-    page.setViewport({ width: 1242, height: 2688 });
-    await page.goto('http://wj.shanghaitech.edu.cn/user/qlist.aspx?sysid=159110074');
+    try {
+        const page = await browser.newPage();
+        page.setViewport({ width: 1242, height: 2688 });
+        await page.goto('http://wj.shanghaitech.edu.cn/user/qlist.aspx?sysid=159110074');
 
-    const getText = async (selector) => {
-        const text = await page.$eval(selector, el => el.innerText);
-        return text.trim();
-    };
-    const type = async (selector, text) => {
-        await page.focus(selector);
-        await page.keyboard.type(text, { delay: Math.random() * 100 + 50 });
-        await delay(Math.random() * 500);
-    };
+        const getText = async (selector) => {
+            const text = await page.$eval(selector, el => el.innerText);
+            return text.trim();
+        };
+        const type = async (selector, text) => {
+            await page.focus(selector);
+            await page.keyboard.type(text, { delay: Math.random() * 100 + 50 });
+            await delay(Math.random() * 500);
+        };
 
 
-    await type('#register-user-name', username);
-    await type('#register-user-password', password);
-    await page.click("#btnSubmit");
-    await page.waitForNavigation();
+        await type('#register-user-name', username);
+        await type('#register-user-password', password);
+        await page.click("#btnSubmit");
+        await page.waitForNavigation();
 
-    const mainform = '#divFolder > div > div > dl:nth-child(1) > a';
-    let text = await getText(mainform);
-    console.log(text);
-    assert(text.startsWith('学生抗原自检结果反馈流程'));
-    await page.click(mainform);
-    await delay(Math.random() * 10000 + 2000);
+        const mainform = '#divFolder > div > div > dl:nth-child(1) > a';
+        let text = await getText(mainform);
+        console.log(text);
+        assert(text.startsWith('学生抗原自检结果反馈流程'));
+        await page.click(mainform);
+        await delay(Math.random() * 10000 + 2000);
 
-    console.log('start fill form');
+        console.log('start fill form');
 
-    const confirm = '#divquestion2 > ul > li > label';
-    text = await getText(confirm);
-    assert.equal(text, '确认');
-    await page.click(confirm);
+        const confirm = '#divquestion2 > ul > li > label';
+        text = await getText(confirm);
+        assert.equal(text, '确认');
+        await page.click(confirm);
 
-    const negative = '#divquestion3 > ul > li:nth-child(1) > label';
-    text = await getText(negative);
-    assert.equal(text, '阴性');
-    await page.click(negative);
+        const negative = '#divquestion3 > ul > li:nth-child(1) > label';
+        text = await getText(negative);
+        assert.equal(text, '阴性');
+        await page.click(negative);
 
-    const id_input = '#q5';
-    await type(id_input, covid_id);
+        const id_input = '#q5';
+        await type(id_input, covid_id);
 
-    const upload = '#uploadFrame4';
-    const [fileChooser] = await Promise.all([
-        page.waitForFileChooser(),
-        page.click(upload),
-    ]);
-    await fileChooser.accept([covid_img]);
+        const upload = '#uploadFrame4';
+        const [fileChooser] = await Promise.all([
+            page.waitForFileChooser(),
+            page.click(upload),
+        ]);
+        await fileChooser.accept([covid_img]);
 
-    await delay(Math.random() * 10000 + 10000);
+        await delay(Math.random() * 10000 + 10000);
 
-    const submit = '#submit_button';
-    await page.click(submit);
+        const submit = '#submit_button';
+        await page.click(submit);
 
-    await delay(Math.random() * 10000 + 10000);
-
-    await browser.close();
+        await delay(Math.random() * 10000 + 10000);
+    } finally {
+        await browser.close();
+    }
 };
 
 (async () => {
@@ -133,14 +135,16 @@ async function submit(covid_img) {
                     await submit(new_path);
                     console.log('submit success');
                 } catch (e) {
+                    console.log(e);
                     console.log('submit failed');
-                    console.log('delay 2 min')
+                    console.log('delay 2 min');
                     await delay(2 * 60 * 1000);
                     try {
                         console.log('try again');
                         await submit(new_path);
                         console.log('submit success');
                     } catch (e) {
+                        console.log(e);
                         console.log('submit failed');
                     }
                 }
